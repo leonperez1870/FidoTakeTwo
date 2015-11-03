df8cc855f1c5dbbc9468173d5120ee8e1ec4e37a
Leoncio <leoncioperez512@gmail.com>
2015-11-03 13:01:34 -0500

    init commit

---
+++ app/controllers/fido_take_two_controller.rb
@@ -0,0 +1,13 @@
+class FidoTakeTwoController < ApplicationController
+	before_action :configure_permitted_parameters, if: :devise_controller?
+  def index
+  end
+
+  def search
+  end
+
+  def review
+  	
+  end
+
+end
